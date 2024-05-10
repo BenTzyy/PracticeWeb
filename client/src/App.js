682c660a12168a3b324d1fa159fa53b2981a3633
AlbernJ. <joaquinalbern@gmail.com>
2024-05-10 15:49:25 +0800

    sana ok na

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 // App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./MainPage";
 import PlayPage from "./PlayPage";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/play" element={<PlayPage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
